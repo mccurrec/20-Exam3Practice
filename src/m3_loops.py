2a8a4196e8e7dc6cdef086d6d6c60c90e45ac5ec
mccurrec <mccurrec@rose-hulman.edu>
2019-01-29 21:47:52 -0500

    done

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Ezrie McCurry.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -46,7 +46,7 @@ def main():
 def run_test_practice_problem3():
     """ Tests the   practice_problem3  function. """
     ###########################################################################
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem3  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -135,7 +135,21 @@ def run_test_practice_problem3():
     # SUGGESTION: Ask an assistant to CHECK your tests to confirm
     #             that they are adequate tests!
     ###########################################################################
+    # Test 15:
+    expected = ([0, 1])
+    actual = practice_problem3(-2, 2, -.3011686789)
+    print()
+    print('Test 15:')
+    print('  Expected:', expected)
+    print('  Actual:  ', actual)
 
+    # Test 16:
+    expected = ([])
+    actual = practice_problem3(-5, 0, 1.24)
+    print()
+    print('Test 16:')
+    print('  Expected:', expected)
+    print('  Actual:  ', actual)
 
 def practice_problem3(start, n, threshold):
     """
@@ -209,7 +223,7 @@ def practice_problem3(start, n, threshold):
       :type threshold: float
     """
     ###########################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ###########################################################################
@@ -217,7 +231,16 @@ def practice_problem3(start, n, threshold):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ###########################################################################
-
+    count = 0
+    k = start
+    returning = []
+    while True:
+        if count >= n:
+            return returning
+        if math.sin(k) + math.cos(k) > threshold:
+            returning = returning + [k]
+            count = count + 1
+        k = k + 1
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
